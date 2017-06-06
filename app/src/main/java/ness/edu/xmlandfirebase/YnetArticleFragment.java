d10c2ad5f3c6b885bf51b9114ceff619b36dd16b
NessCollege <nesscollege2017@gmail.com>
2017-06-06 19:03:06 +0300

    RecyclerView Working added a detail fragment

--- app/src/main/java/ness/edu/xmlandfirebase/YnetArticleFragment.java
+++ app/src/main/java/ness/edu/xmlandfirebase/YnetArticleFragment.java
@@ -9,8 +9,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -43,7 +47,7 @@ public class YnetArticleFragment extends Fragment implements YnetDataSource.OnYn
             @Override
             public void run() {
                 if (e == null)
-                    Toast.makeText(getContext(), data.toString(), Toast.LENGTH_SHORT).show();
+                    recyclerView.setAdapter(new YnetAdapter(data, getActivity()));
                 else
                     Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
             }
@@ -51,5 +55,56 @@ public class YnetArticleFragment extends Fragment implements YnetDataSource.OnYn
 
     }
 
+    static class YnetAdapter extends RecyclerView.Adapter<YnetAdapter.YnetViewHolder>{
+        //Properties:
+        private List<YnetDataSource.Ynet> data;
+        private Context context;
+        private LayoutInflater inflater;
+        //Constructor:
+        public YnetAdapter(List<YnetDataSource.Ynet> data, Context context) {
+            this.data = data;
+            this.context = context;
+            this.inflater = LayoutInflater.from(context);
+        }
+        @Override
+        public YnetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //inflate a single item -> View
+            View v = inflater.inflate(R.layout.ynet_item, parent, false);
+            //return a new ViewHolder
+            return new YnetViewHolder(v);
+        }
+        @Override
+        public void onBindViewHolder(YnetViewHolder holder, int position) {
+            YnetDataSource.Ynet ynet = data.get(position);
+            holder.tvTitle.setText(ynet.getTitle());
+            holder.tvContent.setText(ynet.getDescription());
+            Picasso.with(context).load(ynet.getImage()).into(holder.ivThumbnail);
+        }
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        //View Holder: holds the views
+        class YnetViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            ImageView ivThumbnail;
+            TextView tvTitle;
+            TextView tvContent;
+
+            public YnetViewHolder(View v) {
+                super(v);
+                ivThumbnail = (ImageView) v.findViewById(R.id.ivThumbnail);
+                tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+                tvContent = (TextView) v.findViewById(R.id.tvContent);
+
+                v.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
+    }
 
 }
